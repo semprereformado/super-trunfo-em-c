ce487057bc60a4ba46d23fd08d4e5f1cd2c764c9
Cleber Carvalho <carvalho.anjos@gmail.com>
2025-06-04 17:28:58 -0300

    Exclui uma linha do código fonte

--- super_trunfo.c
+++ super_trunfo.c
@@ -4,7 +4,6 @@
     // Tema 1 - Cadastro das Cartas
     // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
     // Siga os comentários para implementar cada parte do desafio.
-    //Teste larissa
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
